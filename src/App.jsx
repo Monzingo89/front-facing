efceee0e6366d69cf66e5d9f8c266e4d3d936f9d
Noah Villa <noah09villa@gmail.com>
2025-06-24 20:55:57 -0500

    WIP: Save before rebase

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,29 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import RequestAccess from './pages/RequestAccess';
+import RefundPolicy from './pages/RefundPolicy';
+import Footer from './components/Footer';
+import PrivacyPolicy from './pages/PrivacyPolicy';
+import TermsOfService from './pages/TermsOfService';
+import ScrollToTop from './components/ScrollToTop';
+import Support from './pages/Support';
 import './index.css';
 
 function App() {
   return (
     <Router>
+      <ScrollToTop /> {/* ✅ This goes here, outside <Routes> */}
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/request-access" element={<RequestAccess />} />
+        <Route path="/refund-policy" element={<RefundPolicy />} />
+        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+        <Route path="/terms-of-service" element={<TermsOfService />} />
+        <Route path="/support" element={<Support />} />
       </Routes>
+      <Footer />
     </Router>
   );
 }
